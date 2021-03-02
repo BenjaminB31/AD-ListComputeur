a1c696c1b8f45137a2b1d53dbac3becb909816df
Benjamin Blanchard <56205597+BenjaminB31@users.noreply.github.com>
2021-03-02 11:22:31 +0100

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,4 +1,5 @@
 Import-Module ActiveDirectory
 $Domaine = "domain.local"
-$ListeOrdinateur = Get-ADComputer -Filter * -Properties LastLogonTimeStamp | FT Name,@{n='LastLogon';e={[DateTime]::FromFileTime($_.LastLogonTimeStamp)}}
+$ListeOrdinateur = Get-ADComputer -Filter * -Properties LastLogonTimeStamp | Select-Object Name,@{n='LastLogon';e={[DateTime]::FromFileTime($_.LastLogonTimeStamp)}}
 Write-Output $ListeOrdinateur
+$ListeOrdinateur | Export-CSV C:\Users\ADMIN\Desktop\ComputerList.csv -NoTypeInformation -Encoding UTF8
